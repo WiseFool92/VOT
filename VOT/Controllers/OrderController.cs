b2bb1b2505b8119d2b14e98f420119c478541155
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-22 10:17:03 -0700

    initial ordercontroller framework

---
+++ VOT/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using VOT.Models;
+using System.Collections.Generic;
+
+namespace VOT.Controllers
+{
+  public class OrderController : Controller
+  {
+    
+  }
+}
