1ed66b1d904bbad3874d8f33186c5750c4885360
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-22 11:00:44 -0700

    populate order controller w/requests

--- VOT/Controllers/OrderController.cs
+++ VOT/Controllers/OrderController.cs
@@ -6,6 +6,29 @@ namespace VOT.Controllers
 {
   public class OrderController : Controller
   {
-    
+    [HttpGet("/vendor/{vendorId}/order/new")]
+    public ActionResult New (int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendor/{vendorId}/order/{orderId}")]
+    public ActionResult Show (int vendorId, int orderId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      Order order = Order.Find(orderId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("vendor", vendor);
+      model.Add("order", order);
+      return View(model);
+    }
+
+    [HttpPost("/order/delete")]
+    public ActionResult DeleteAll ()
+    {
+      Order.ClearAll();
+      return View();
+    }
   }
 }
